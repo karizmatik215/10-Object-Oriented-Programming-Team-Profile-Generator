8577767edf6376c83a823507426d79b468a920c4
Juan Sanchez <juan.sanchez@phila.gov>
2021-03-04 19:53:25 -0500

    test Engineer github account and role

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,30 @@
+const { TestScheduler } = require('@jest/core');
+const Engineer = require('../lib/Engineer');
+
+test('Can set github account', () => {
+  const testGithubAccount = 'randomusername';
+  const employee = new Engineer(
+    'Alec',
+    2,
+    'alec@fakemail.com',
+    testGithubAccount
+  );
+  expect(employee.github).toBe(testGithubAccount);
+});
+
+test('getRole() method should return Engineer', () => {
+  const testGetRole = 'Engineer';
+  const employee = new Engineer(
+    'Alec',
+    2,
+    'alec@fakemail.com',
+    'randomusername'
+  );
+  expect(employee.getRole()).toBe(testGetRole);
+});
+
+test('getGithub() method should get username', () => {
+  const testGitHubUser = 'randomusername';
+  const employee = new Engineer('Alec', 2, 'alec@fakemail.com', testGitHubUser);
+  expect(employee.getGithub()).toBe(testGitHubUser);
+});
